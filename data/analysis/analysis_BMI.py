91c562391e4ae23fac3b2f2dcdc7495cf590039a
Matt Chan <chanmhy@mcmaster.ca>
2015-02-21 14:05:27 -0500

    added adultBMI

---
+++ data/analysis/analysis_BMI.py
@@ -0,0 +1,43 @@
+__author__ = 'mattchan'
+
+
+from pandas import read_csv
+import numpy as np
+csv_data = read_csv("data/analysis/AdultBMI.csv")
+
+csv_data = csv_data.values
+results = []
+
+for i in csv_data:
+    coords = [int(j) for j in i[-2].split(".")]
+    results.append(coords)
+
+a = np.array(results)
+
+past0 = a[0, 2]
+temp = []
+
+with open("AdultBMI_out.csv", "w") as fh:
+    for key, i in enumerate(csv_data):
+        cur0 = a[key, 2]
+
+        if cur0 != past0:
+            # new coord in first 2
+            msg = "\"{0}\",{1},{2},{3},{4},{5}\n".format(i[0], i[1], i[2], i[3], i[4], ",".join(map(str, temp)))
+            # print msg
+            fh.write(msg)
+            temp = []
+
+        if i[-1] == "F":
+            temp.append(-1)
+        elif i[-1] == "..":
+            temp.append(-1)
+        else:
+            temp.append(i[-1])
+        past0 = cur0
+
+    msg = "\"{0}\",{1},{2},{3},{4},{5}\n".format(i[0], i[1], i[2], i[3], i[4], ",".join(map(str, temp)))
+    # print msg
+    fh.write(msg)
+
+
