33252239efc315e775957233e4df81a5d14f67b7
anarian <jonathanlaw2@outlook.com>
2015-02-21 10:01:03 -0500

    Database Query Framework
    Working database querying section, fixed float/decimal input for HTML.

--- results.php
+++ results.php
@@ -11,15 +11,18 @@
 	$weight = $_POST["weight"];
 	$self_rate = $_POST["self-rate"];
 	$internet_use = $_POST["internet-use"];
+
+    $internet_use_result = 0.0;
+
 	
 	$connection = new mysqli($servername, $username, $password, $dbname);
 	
 	if($connection->connect_error) {
 		die("$connection->connect_errno: $connection->connect_error");
 	}
-	
+
 	$stmt = $connection->stmt_init();
-	if(!$stmt->prepare("SELECT OncePerDay, OncePerWeek, OncePerMonth, LessThanMonth FROM InternetUse WHERE Year = 2009 AND AgeRange_Lower <= ? AND AgeRange_Higher >= ?"))
+	if(!$stmt->prepare("SELECT OncePerDay, OncePerWeek, OncePerMonth, LessThanMonth FROM InternetUse WHERE Year = 2009 AND AgeRange_Lower <= ? AND AgeRange_Higher >= ? AND AgeRange_Higher != 100"))
 	{
 		print("Failed to prepare statement\n");
 	}
@@ -29,9 +32,26 @@
 
         $stmt->bind_result($onceperday, $onceperweek, $oncepermonth, $lessthanmonth);
 
-        while($stmt->fetch()) {
-            printf("%s %s %s %s\n", $onceperday, $onceperweek, $oncepermonth, $lessthanmonth);
+        $stmt->fetch();
+
+        switch($internet_use) {
+            case "once-per-day":
+                $internet_use_result = floatval($onceperday);
+                break;
+            case "once-per-week":
+                $internet_use_result = floatval($onceperweek);
+                break;
+            case "once-per-month":
+                $internet_use_result = floatval($oncepermonth);
+                break;
+            case "less-per-month":
+                $internet_use_result = floatval($lessthanmonth);
+                break;
+            default:
+                $internet_use_result = -1.0;
         }
+
+        echo "You are similar to " . $internet_use_result . "% of Canadians!";
 
         $stmt->close();
 
