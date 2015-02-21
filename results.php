3459ae7c68927275f5766a3ca2a5d82eaa511149
anarian <jonathanlaw2@outlook.com>
2015-02-20 20:55:21 -0500

    Fonts and style changes
    Few styling changes, added fonts.

---
+++ results.php
@@ -0,0 +1,12 @@
+<?php
+
+	$age = $_POST["age"];
+	$gender = $_POST["gender"];
+	$height = $_POST["height"];
+	$weight = $_POST["weight"];
+	$self_rate = $_POST["self-rate"];
+	$internet_use = $_POST["internet-use"];
+	
+	
+	
+?>
